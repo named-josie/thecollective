595fe745b2b8f481321150008b326a17703d82e0
named.josie <named.josie@gmail.com>
2023-02-07 16:52:10 -0500

    navbar updates

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,25 +1,23 @@
-
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from './Components/NavBar';
 import Mission from './Pages/Mission';
 import Home from './Pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      <Router>
-        <NavBar />
-        <main>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/mission' element={< Mission />} />
-            {/* <Route path='*' element={<Four0Four />} />  */}
-          </Routes>
-        </main>
-      </Router> 
-       
+    <div className='App'>
+      <header className='App-header'>
+        <Router>
+          <NavBar />
+          <main>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/mission' element={<Mission />} />
+              {/* <Route path='*' element={<Four0Four />} />  */}
+            </Routes>
+          </main>
+        </Router>
       </header>
     </div>
   );
